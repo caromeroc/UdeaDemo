dfe9c9e1105eb0d7cdb3b7d371a23647c1aae463
caromeroc <ing.carlos.romero@outlook.com>
2022-09-07 21:49:13 -0500

    feat: Front Segunda parte

--- src/main/java/com/co/udea/mintic/UdeaDemo/controller/ControllerFrontEnd.java
+++ src/main/java/com/co/udea/mintic/UdeaDemo/controller/ControllerFrontEnd.java
@@ -1,10 +1,19 @@
 package com.co.udea.mintic.UdeaDemo.controller;
 
+import com.co.udea.mintic.UdeaDemo.repository.EntityPersona;
+import com.co.udea.mintic.UdeaDemo.services.ServicePersona;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ControllerFrontEnd {
+
+    @Autowired
+    ServicePersona servicePersona;
 
     @GetMapping (path = "/")
     public String home(){
@@ -13,7 +22,10 @@ public class ControllerFrontEnd {
     }
 
     @GetMapping (path = "/pagina2")
-    public String pagina2(){
+    public String pagina2(Model modelo){
+
+        List<EntityPersona> listPersonas = servicePersona.listarTodosJPA();
+        modelo.addAttribute("personas", listPersonas);
 
         return "pagina2";
     }
